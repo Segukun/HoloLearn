39357f7a57097e63bf53b09003fefe147d12f23b
セカンド <Segudg@gmail.com>
2025-10-23 15:27:46 -0300

    Cosas chicas

--- frontend/src/components/layout/layout.jsx
+++ frontend/src/components/layout/layout.jsx
@@ -5,6 +5,7 @@ import "../../styles/components/layout.css";
 
 export default function Layout() {
   return (
+    <>
     <div className="layout">
       <Header />
       <main className="main">
@@ -12,5 +13,6 @@ export default function Layout() {
       </main>
       <Footer />
     </div>
+    </>
   );
 }
